6312384a0486729261d7ac56636e19e0277e96cf
heyitskaya <ni23x@mtholyoke.edu>
2016-03-09 23:48:34 -0500

    Testing Shadow for text

--- Assets/Scripts/Tools/SpecialActions/SpecialActions_Bottle.cs
+++ Assets/Scripts/Tools/SpecialActions/SpecialActions_Bottle.cs
@@ -13,8 +13,8 @@ public class SpecialActions_Bottle : SpecialActions
 		bool destroy = false;
 		if(actionTag.Equals("RevealWhiteRagObject"))
 		{
-				destroy=true;
-				Instantiate(Rag,transform.position,Quaternion.identity);
+			destroy=true;
+			Instantiate(Rag,transform.position,Quaternion.identity);
 			return;
 		}
 		if(destroy)
