8ecfda8427a30995b78c115b0b2285267ab074f3
heyitskaya <ni23x@mtholyoke.edu>
2016-04-07 21:44:20 -0400

    Fixed null reference exception

--- Assets/Scripts/Tools/NoahNavPlane.cs
+++ Assets/Scripts/Tools/NoahNavPlane.cs
@@ -6,7 +6,8 @@ public class NoahNavPlane : MonoBehaviour, IPointerClickHandler {
 
 	NavMeshAgent Player;
 
-	public bool flipped = false;
+	public bool flipped = false; //when sadie is facing right flipped is false
+	public bool startMoving = false;
 	private SpeechBubble speechBubble;
 
 	// Use this for initialization
@@ -18,7 +19,27 @@ public class NoahNavPlane : MonoBehaviour, IPointerClickHandler {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Player.remainingDistance <= Player.stoppingDistance) {
+			if (startMoving ) {
+				startMoving = false;
+				Debug.Log ("Stopped moving");
+				//check x then flip
+
+				if (Player.transform.position.x < 4.5f && Player.transform.position.x > 0f && startMoving==false) { //and we have stopped moving
+					Debug.Log("flip1");
+					if (flipped) { //if sadie is facing left
+						Flip (); //flip it sadie is facing right
+					}
+
+				} 
+				if (Player.transform.position.x < 11.333f && Player.transform.position.x > 8f && startMoving == false) {
+					Debug.Log ("flip2");
+					if (!flipped) { //if sadie is facing right
+						Flip (); //flip it sadie is now facing left
+					}
+				}
+			}
+		}
 	}
 
 	void OnMouseUp(){
@@ -33,6 +54,7 @@ public class NoahNavPlane : MonoBehaviour, IPointerClickHandler {
 	public void OnPointerClick (PointerEventData eventData)
 	{
 		if (!UIManager._instance.paused){
+			startMoving = true;
 			#if DEBUG
 			Debug.Log("Clicked on Nav Floor.");
 			#endif
@@ -40,18 +62,20 @@ public class NoahNavPlane : MonoBehaviour, IPointerClickHandler {
 			Vector3 destination = eventData.pointerCurrentRaycast.worldPosition;
 			Player.GetComponent<NavMeshAgent> ().CalculatePath (destination, path);
 			//if(path.status == NavMeshPathStatus.PathComplete){
-			if (destination.x > Player.transform.position.x) {
-				if (flipped) {
-					Flip ();
+			if (destination.x > Player.transform.position.x) { //destination is to right of Sadie   sadie   destination
+				Debug.Log("We should be in here");
+				if (flipped) { // if sadie is facing left
+					Flip (); //sadie is now facing right
 				}
-			} else {
-				if (!flipped) {
-					Flip ();
+			} else { //when destination is on Sadie's left
+				if (!flipped) { //sadie is facing right
+					Flip (); //sadie is now facing left
 				}
 			}
 
 			Player.SetDestination (destination);
 			//}
+
 		}
 	}
 }
